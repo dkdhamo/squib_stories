100d9a30dcee55f44782fbce028091d6b0afce1f
dkdhamo <dkdhamkannan2802@gmail.com>
2024-02-25 20:47:50 +0530

    router change

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Land from '../views/Land.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    
+    { path: "/:catchAll(.*)", redirect: "/" },
     {
       path: '/', //brands/:brand/:id
       name: 'Land',
@@ -22,8 +24,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    { path: "*", redirect: "/" }
+    }
   ]
 })
 
