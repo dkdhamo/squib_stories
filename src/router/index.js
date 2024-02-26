38060f1a2798c4a4843bcdef81d990b52af93942
dkdhamo <dkdhamkannan2802@gmail.com>
2024-02-26 12:37:33 +0530

    redirects changes

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Land from '../views/Land.vue'
 
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     
-    { path: "/:catchAll(.*)", redirect: "/" },
+  
     {
       path: '/', //brands/:brand/:id
       name: 'Land',
@@ -24,7 +26,8 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    { path: "/:catchAll(.*)", redirect: "/" }
   ]
 })
 
